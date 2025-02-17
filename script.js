d8f0c02d46b5acd7d46d6dfe863d2b1df3049e0f
syazidkr <111582922+syazidkr@users.noreply.github.com>
2025-02-17 15:14:21 +0000

    Modify files

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const signupSectionTitle = document.getElementById("section-title-sign-up")
 const loginSectionTitle = document.getElementById("section-title-log-in")
+const signupButton = document.getElementById("sign-up-btn")
+const loginButton = document.getElementById("log-in-btn")
 
 signupSectionTitle.addEventListener("click", () => {
         const computedStyle = window.getComputedStyle(signupSectionTitle);
         if (computedStyle.backgroundColor === "rgb(211, 211, 211)") {
             signupSectionTitle.style.backgroundColor = "white";
             loginSectionTitle.style.backgroundColor = "lightgrey";
+            
         }
     });
 
